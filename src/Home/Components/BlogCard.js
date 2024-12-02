15da786eeada69318e08768585d0298be9e1a50d
ctokkm <2271488.hansung.ac.kr>
2024-12-02 19:53:50 +0900

    홈에서 PostDetail로 넘어가도록 토대 마련

--- src/Home/Components/BlogCard.js
+++ src/Home/Components/BlogCard.js
@@ -1,9 +1,17 @@
 import React from "react";
+import { useNavigate } from "react-router-dom"; // React Router 사용
 import "../Styles/BlogCard.css";
 
-const BlogCard = ({ title, author, date, description, likes, image }) => {
+const BlogCard = ({ id, title, author, date, description, likes, image }) => {
+  const navigate = useNavigate();
+
+  // 카드 클릭 시 상세 페이지로 이동
+  const handleCardClick = () => {
+    navigate(`/posts/${id}`); // 게시물의 ID를 URL에 포함하여 상세 페이지로 이동
+  };
+
   return (
-    <div className="blog-card">
+    <div className="blog-card" onClick={handleCardClick} style={{ cursor: "pointer" }}>
       <img
         src={image || process.env.PUBLIC_URL + "/로고3.png"} // public 폴더에서 이미지 참조
         alt={title}
